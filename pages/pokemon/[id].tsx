fdf91772df263562611befe809cf1b8bdd532cb3
Fabian Molina <molinafabian11b@gmail.com>
2023-01-03 23:06:16 -0500

    feat: Dynamic routes were generated for each pokemon✨
    Signed-off-by: Fabian Molina <molinafabian11b@gmail.com>

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,17 +1,12 @@
 import { GetStaticProps, GetStaticPaths, NextPage } from 'next'
-import { useRouter } from 'next/router'
-import { pokeLimit } from '../../api'
-
 import { MainLayout } from '../../components/layouts'
+import { Pokemon } from '../../interfaces'
 
 interface Props {
-  id: string
-  name: string
+  pokemon: Pokemon
 }
-const PokemonPage: NextPage<Props> = ({ id, name }) => {
-  const router = useRouter()
-  console.log(router.query)
-
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+  const { name, id } = pokemon
   return (
     <MainLayout>
       <h1>
@@ -25,28 +20,30 @@ const PokemonPage: NextPage<Props> = ({ id, name }) => {
 // Solo se usa junto con getStaticProps, nunca con server side rendering u otra forma de reenderizado
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await pokeLimit()
-  console.log(res)
-
-  type PokeType = {
-    name?: string
-    id: string
-  }
-
-  const paths = res.map((data: PokeType) => ({
-    params: { id: data.id },
-  }))
+  // Crea el Array {id} de 150 pokemons
+  const pokemons150 = [...Array(150)].map((value, index) => `${index + 1}`)
 
   return {
-    paths,
-
-    // {fallback: false} si ingresa a otras rutas no definidas dara un 404
+    paths: pokemons150.map((id) => ({
+      params: { id },
+    })),
+    // {fallback: false} si ingresa a otras rutas no definidas en {params} dara un 404
     fallback: false,
   }
 }
-export const getStaticProps: GetStaticProps = async () => {
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string }
+
+  //Llamada a la API con el ID dinamico
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const data: Pokemon = await res.json()
+
   return {
-    props: { id: 1, name: 'Bulbasaur' },
+    props: {
+      pokemon: data,
+    },
   }
 }
+
 export default PokemonPage
